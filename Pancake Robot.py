b761f6d6b5b902335715d0a8e0a2c21f029541be
Jacob Campbell <jacob.campbell.1@gmail.com>
2019-11-23 20:41:06 -0500

    Untested python script
    just my first attempt trying gitkraken

---
+++ Pancake Robot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+
+from ev3dev2.sound import Sound
+
+sound = Sound()
+
+sound.speak('Pancake robot, come and get them while they are hot.', '-a 200 -s 130', 50, 0)   #say 'I am done' -a 200 means
+
