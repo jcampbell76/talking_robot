65275f34f875c6854c7c3eeb0994e562720ed334
WavesFLL <jcotellesso@gmail.com>
2019-11-25 19:19:01 -0500

    Print added
    Print added; says something along the lines of "print added"

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ pancake_lyrics_1 = 'Pancake robot, come and get them while there hot'
 #say string with max volume (-a 200) and speed -s
 opts = '-a 200 -s 200 '   
 sound.speak(pancake_lyrics_1, espeak_opts=opts+'-ven')   
-
+print("Here is a print!")
